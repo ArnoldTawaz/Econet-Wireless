308f91a81f6f1430bc0bfd3f8fdfbde116304732
Tawanda Marondedze <marondedzet@eus.net.payserv.co.zw>
2018-05-07 09:54:33 +0200

    Added Test

---
+++ electronic-payments-business/src/Tests/com/econetwireless/epay/business/config/EpayBusinessConfigTest.java
@@ -0,0 +1,26 @@
+package com.econetwireless.epay.business.config;
+
+import com.econetwireless.epay.business.integrations.api.ChargingPlatform;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EpayBusinessConfigTest {
+    private EpayBusinessConfig C_Test = new EpayBusinessConfig();
+    @Test
+    public void creditsService() throws Exception{
+
+    }
+
+    @Test
+    public void enquiriesService() throws Exception{
+    }
+
+    @Test
+    public void reportingService() throws Exception{
+    }
+
+    @Test
+    public void partnerCodeValidator() throws Exception{
+    }
+}
